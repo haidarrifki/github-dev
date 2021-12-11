48aa862403656a41a850c6415660a79943b73ce0
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2021-12-11 15:56:38 +0300

    Github user finder project first commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Sass Task
+function scssTask() {
+  return src('src/scss/style.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('src/css', { sourcemaps: '.' }));
+}
+
+// Browsersync
+function browserSyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0',
+      },
+    },
+  });
+  cb();
+}
+
+function browserSyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch('*.html', browserSyncReload);
+  watch(
+    ['src/scss/**/*.scss'],
+    series(scssTask, browserSyncReload)
+  );
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, browserSyncServe, watchTask);
+
+// Build Gulp Task
+exports.build = series(scssTask);
---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+import { Github } from "./github.js"
+import { UI } from "./ui.js"
+
+const searchUser = document.getElementById('search-user')
+const searchBtn = document.querySelector('.search-btn')
+const github = new Github()
+const ui = new UI()
+
+
+eventlisetner()
+function eventlisetner(e) {
+    searchBtn.addEventListener('click', getData)
+}
+
+function getData(e) {
+    let username = searchUser.value.trim()
+
+    if (username === "") {
+        confirm("Please enter a valid username.")
+
+    } else {
+        github.getGithubData(username)
+        .then(response => {
+            if (response.user.message === "Not Found") {
+                ui.showError('danger', 'User not found.')
+            } else {
+                ui.showUserInfo(response.user)
+                ui.showRepoInfo(response.repo)
+            }
+        }).catch(error => console.log(error))
+    }
+
+    ui.clearInput()
+    e.preventDefault()
+}
