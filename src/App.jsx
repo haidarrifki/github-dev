9348173eb5d9aed2dd2e95465c57419a34224fb6
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2022-10-05 21:37:45 +0300

    feat(repo): project infrastructure converted to reactjs (#4)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Github User Finder</h1>
+    </>
+  );
+}
+
+export default App;
